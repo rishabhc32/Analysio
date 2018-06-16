3b7dd3b2bcfaeb01cbd49e337950f64c1818a8e4
Rishabh Chauahn <rishabhc32@gmail.com>
2018-06-17 00:50:08 +0300

    added  python execution function

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const {exec} = require("child_process");
+const express = require("express")
 
-exec('python3 diabetes.py', (err, stdout, stderr) => {
-    if(err) {
-        throw err;
-    }
+const app = express()
+app.use(express.static('public'))
 
-    if(stdout.includes('Done')) {
-        console.log('Done');
-        exec('ls', (err, stdout, stderr) => {
-            console.log(stdout);
-        })
-    }
-    console.log('stderr: ', stderr);
+var analyze = function () {
+    exec('python3 diabetes.py', (err, stdout, stderr) => {
+        if(err) {
+            throw err;
+        }
+
+        if(stdout.includes('Done')) {
+            console.log('Done');
+            exec('ls', (err, stdout, stderr) => {
+                console.log(stdout);
+            })
+        }
+        console.log('stderr: ', stderr);
+    })
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 })
+
+app.get('/analyze', (req, res) => {
+    analyze();
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.listen(4433, () => console.log('Started server'))
