e3a1f7e20783dd1fca452fedd5ec687e6754ed56
Rishabh Chauahn <rishabhc32@gmail.com>
2018-06-17 00:54:14 +0300

    added public/js folder

---
+++ public/js/init.js
@@ -0,0 +1,14 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+$('.carousel-slider').slider({full_width: true});//slider init
+$(document).ready(function(){
+  $('.slider').slider();
+});
+      
+
